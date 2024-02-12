70ff671c9f0a8b35026e34c67d9012fbac337388
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-12 23:35:23 +0100

    Redux installation and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <Provider store={store}>
+
+      </Provider>
   </React.StrictMode>,
 )
