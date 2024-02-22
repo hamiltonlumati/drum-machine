5ee5c91efc8695e113793f38c332f7d772dd45d4
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-22 17:40:44 +0100

    "buttons"

---
+++ src/components/buttons.tsx
@@ -0,0 +1,19 @@
+function Buttons(){
+
+
+    return (
+        <div className="grid grid-cols-3 gap-2 w-64">
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+
+        </div>
+    )
+}
+export default Buttons;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import Screen from './layout/screen';
+import Buttons from './components/buttons';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
-        <Screen />
+        <Buttons />
       </Provider>
   </React.StrictMode>,
 )
