9019e51bfb3a1314eac043cac4b70a335915bc50
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-23 16:48:03 +0100

    reducer definition

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -2,16 +2,16 @@ function Buttons(){
 
 
     return (
-        <div className="grid grid-cols-3 gap-2 w-64">
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
-            <button className="box-border h-32 w-32 p-4 bg-red-500">Test</button>
+        <div className="grid grid-cols-3 gap-2 w-52">
+            <button className="box-border h-16 w-16 p-4 bg-red-500" >Q</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">W</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">E</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">A</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">S</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">D</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">Z</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">X</button>
+            <button className="box-border h-16 w-16 p-4 bg-red-500">C</button>
         </div>
     )
 }
--- src/features/drummer/drumSlice.tsx
+++ src/features/drummer/drumSlice.tsx
@@ -1,2 +1,55 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface DrumState{
+    key: string
+}
+
+const initialState: DrumState = {
+    key: "none"
+}
+
+export const drumSlice = createSlice({
+    name: 'drum',
+    initialState,
+    reducers:{
+        Q: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3');
+            audio.play();
+            state.key = "Heater 1";
+        },
+        W: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3');
+            audio.play();
+        },
+        E: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3');
+            audio.play();
+        },
+        A: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3');
+            audio.play();
+        },
+        S: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3');
+            audio.play();
+        },
+        D: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3');
+            audio.play();
+        },
+        Z: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3');
+            audio.play();
+        },
+        X: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3');
+            audio.play();
+        },
+        C: (state) => {
+            var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3');
+            audio.play();
+
+        }
+    }
+})
