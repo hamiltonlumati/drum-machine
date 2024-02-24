45afde61eb6bf6dd7b8f55cf7fd53935f06511a1
hamiltonlumati <hamiltonlumati@gmail.com>
2024-02-24 23:28:00 +0100

    Keyboard update

--- src/components/keys.tsx
+++ src/components/keys.tsx
@@ -1,68 +1,73 @@
 import { useDispatch } from "react-redux";
 import {Q, W, E, A, S, D, Z, X, C} from '../features/drummer/drumSlice'
+import { useEffect } from "react";
+
+declare global {
+    interface WindowEventMap {
+        keydown: React.KeyboardEvent<HTMLInputElement>
+    }
+}
+
+
 
 function Keys(){
     const dispatch = useDispatch();
 
-    var addEvent = document.addEventListener ? function(target: DocumentEventMap,type: String, action: boolean){
-        if(target){
-            target.addEventListener(type,action,false);
-        }
-    } : function(target: Document,type: String,action: boolean){
-        if(target){
-            target.attachEvent('on' + type,action,false);
-        }
-    }
-    
-    addEvent(document,'keydown',function(e){
-        e = e || window.event;
-        var key = e.which || e.keyCode;
-        //Q
-        if(key===81){
+    const handleUserKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const { key } = event
+        if(key==='Q'){
             dispatch(Q());
         }
 
         //W
-        if(key===87){
+        if(key==='W'){
             dispatch(W());
         }
 
         //E
-        if(key===69){
+        if(key==='E'){
             dispatch(E());
         }
 
         //A
-        if(key===65){
+        if(key==='A'){
             dispatch(A());
         }
 
         //S
-        if(key===83){
+        if(key==='S'){
             dispatch(S());
         }
 
         //D
-        if(key===68){
+        if(key==='D'){
             dispatch(D());
         }
 
         //Z
-        if(key===90){
+        if(key==='Z'){
             dispatch(Z());
         }
 
         //X
-        if(key===88){
+        if(key==='X'){
             dispatch(X());
         }
 
         //C
-        if(key===67){
+        if(key==='C'){
             dispatch(C());
         }
-    });
-
+    }
+    
+    useEffect(() => {
+        window.addEventListener('keydown', handleUserKeyPress)
+    
+        return () => {
+            window.removeEventListener('keydown', handleUserKeyPress)
+        }
+    })
+    
     return (
         <div className="grid grid-cols-3 gap-2 w-52">
             <button className="box-border h-16 w-16 p-4 bg-red-500" onClick={()=> dispatch(Q())}>Q</button>
