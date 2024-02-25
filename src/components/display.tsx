42e5180d4b8b92d3918c8afb8a634b25f7b14ae9
Hamilton Lumati <hamiltonlumati@gmail.com>
2024-02-25 23:45:41 +0100

    FreeCodeCamp requirements

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 function Display(){
     const key = useSelector((state: RootState) => state.drum.key)
     return(
-        <div className="w-52 text-center h-10 bg-red-500 basis-10 py-2">
+        <div className="w-52 text-center h-10 bg-red-500 basis-10 py-2" id="display">
             {key}
         </div>
     )
